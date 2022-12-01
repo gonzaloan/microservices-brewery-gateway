27fa4395df8bbdaec9bc6823b52d63c301d0a445
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2022-12-01 12:26:09 -0300

    Adds sample routing example

---
+++ src/main/java/cl/gonmunoz/microservicesbrewerygateway/config/GoogleConfiguration.java
@@ -0,0 +1,21 @@
+package cl.gonmunoz.microservicesbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfiguration {
+
+    @Bean
+    public RouteLocator googleConfig(RouteLocatorBuilder builder){
+    return builder.routes()
+            .route(r -> r.path("/googlesearch2")
+                    .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                    .uri("https://google.com").id("google"))
+            .build();
+    }
+}
