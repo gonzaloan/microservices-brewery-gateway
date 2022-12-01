eae68a2e54e0356cee9bc6004a1738e52f1d1cf8
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2022-12-01 11:56:33 -0300

    Initial commit, project creation

---
+++ src/main/java/cl/gonmunoz/microservicesbrewerygateway/MicroservicesBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package cl.gonmunoz.microservicesbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesBreweryGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/gonmunoz/microservicesbrewerygateway/MicroservicesBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gonmunoz.microservicesbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
